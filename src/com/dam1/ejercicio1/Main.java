d0d012e2771ddd805d9432a60184d8dee433f7bf
Yago Simoes <ysimoesfernandez@danielcastelao.org>
2021-10-08 09:41:53 +0200

    añadido sout

---
+++ src/com/dam1/ejercicio1/Main.java
@@ -0,0 +1,8 @@
+package com.dam1.ejercicio2;
+public class Main{
+    public static void main (String[]args) {
+        System.out.println ("Ejercicio1");
+
+    }
+
+}
---
+++ src/com/dam1/ejercicio2/nueva.java
@@ -0,0 +1,9 @@
+package com.dam1.ejercicio2;
+public class Main{
+    public static void main (String[]args) {
+        System.out.println ("Nueva");
+
+    }
+
+}
+
